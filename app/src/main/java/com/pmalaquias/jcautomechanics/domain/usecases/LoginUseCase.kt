b866f2a3c07f5243c5750e8e0faf0746318b1ee9
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2024-07-08 15:11:49 -0300

    feat: Improve user authentication in LoginUseCase

--- app/src/main/java/com/pmalaquias/jcautomechanics/domain/usecases/LoginUseCase.kt
+++ app/src/main/java/com/pmalaquias/jcautomechanics/domain/usecases/LoginUseCase.kt
@@ -2,14 +2,36 @@ package com.pmalaquias.jcautomechanics.domain.usecases
 
 import com.pmalaquias.jcautomechanics.domain.entities.User
 import com.pmalaquias.jcautomechanics.domain.repositories.IUserRepository
+import com.pmalaquias.jcautomechanics.presentation.protocols.AuthResult
 
+/**
+ * Defines the use case for user login functionality.
+ *
+ * @property userRepository Provides access to user-related data operations.
+ */
 class LoginUseCase(private val userRepository: IUserRepository) {
+
+    /**
+     * Attempts to authenticate a user with the provided username and password.
+     *
+     * @param username The username of the user attempting to log in.
+     * @param password The password of the user attempting to log in.
+     * @return The authenticated [User] object if credentials are valid, null otherwise.
+     */
     fun execute(username: String, password: String): User? {
         val user = userRepository.getUserByUsername(username) ?: return null
         return if (user.password == password) user else null
     }
 
-    fun login(username: String, password: String): Any {
-        return userRepository.login(username, password)
+    /**
+     * Operator function to provide a more idiomatic way of performing user login.
+     * It wraps the result of the login attempt in an [AuthResult].
+     *
+     * @param username The username of the user attempting to log in.
+     * @param password The password of the user attempting to log in.
+     * @return [AuthResult.Success] with the user if credentials are valid, [AuthResult.Error] otherwise.
+     */
+    operator fun invoke(username: String, password: String): AuthResult {
+        return userRepository.login(username, password).let { AuthResult.Success(it) } ?: AuthResult.Error("Invalid credentials")
     }
 }
