ccf723771efc0bd8c47f8599a46949b02cc60dd6
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2024-07-26 18:28:34 -0300

    feat: Add UserProfilePhoto and WorkshopProfilePhoto composable functions

--- app/src/main/java/com/pmalaquias/jcautomechanics/ui/pages/home/components/ImageProfile.kt
+++ app/src/main/java/com/pmalaquias/jcautomechanics/ui/pages/home/components/ImageProfile.kt
@@ -5,23 +5,39 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Store
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.graphics.shapes.CornerRounding
 import androidx.graphics.shapes.RoundedPolygon
 import androidx.graphics.shapes.star
+import com.pmalaquias.jcautomechanics.ui.theme.JCAutoMechanicsTheme
 
 /**
  * A composable function that defines the layout and behavior of the ImageProfile component within the application.
  * This component creates a hexagon-shaped profile image using a custom [RoundedPolygonShape] and applies various
  * styling properties such as size, background color, clipping, and border.
+ *
+ * @param photoUrl The URL of the profile photo. Defaults to an empty string.
+ * @param modifier A [Modifier] applied to the [Box] layout, allowing for customization of its size, padding, etc.
+ * @param icon An [ImageVector] representing the icon to be displayed in the center of the profile image. Defaults to [Icons.Default.Person].
  */
 @Composable
-fun ImageProfile() {
+fun ImageProfile(
+    photoUrl: String = "",
+    modifier: Modifier = Modifier,
+    icon: ImageVector = Icons.Default.Person
+) {
 
     // Create a hexagon shape with 8 points and corner rounding of 0.2f
     val hexagon = remember { RoundedPolygon.star(8, rounding = CornerRounding(0.2f)) }
@@ -31,12 +47,33 @@ fun ImageProfile() {
 
     // Define a Box composable with specified size, background color, clipping, and border
     Box(
-        modifier = Modifier
-            .size(150.dp)
-            .background(MaterialTheme.colorScheme.primary, shape = clip)
-            .clip(clip)
-            .border(1.dp, MaterialTheme.colorScheme.onPrimary, CircleShape)
+        contentAlignment = Alignment.Center,
     )
+    {
+        Box(
+            modifier = Modifier
+                .size(150.dp)
+                .background(MaterialTheme.colorScheme.primary, shape = clip)
+                .clip(clip)
+                .border(1.dp, MaterialTheme.colorScheme.onPrimary, CircleShape),
+
+
+            )
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+            modifier = Modifier.size(100.dp),
+            tint = MaterialTheme.colorScheme.onPrimary
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewImageProfile() {
+    JCAutoMechanicsTheme {
+        ImageProfile()
+    }
 }
 
 /**
@@ -58,5 +95,7 @@ fun UserProfilePhoto(photoUrl: String = "") {
  */
 @Composable
 fun WorkshopProfilePhoto(photoUrl: String = "") {
-    ImageProfile()
+    ImageProfile(
+        icon = Icons.Default.Store
+    )
 }
